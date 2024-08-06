a5cdb52b1daba65e68132829c57486807d8ed074
radwa <radwa.taha82621@gmail.com>
2024-08-06 21:39:01 +0300

    modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,20 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'modal';
+
+  isClicked: boolean = false;
+  imgSrc: string = '';
+  //  global
+  open(src: string) {
+    console.log(src);
+    this.imgSrc = src;
+    this.isClicked = true;
+  }
+  close() {
+    this.isClicked = false;
+  }
 }
